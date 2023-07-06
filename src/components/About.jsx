8ea9a19cb59e223c14c790b99a3c0703bdff0981
Manish Panda <mkpgtr@gmail.com>
2023-07-06 05:41:07 +0530

    third commit

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,18 +4,59 @@ import { useFormContext } from '../context/FormContext'
 const About = () => {
     const {formValues} = useFormContext()
   return (
-<div className='m-auto w-[50%] text-center mt-5 text-3xl '>
-  I built this as part of an assignment for React Js.
+<div className='m-auto w-[60%] text-center mt-5 text-2xl '>
+  I built this as part of an assignment for React Js. <span className='text-sm'>(Click on About to return anytime here)</span>
 
- <p className='italic'>
+  <p className="italic mt-5">
+  Here is : <a className='text-red-600' href='/form'>The Assignment(Form)  </a>
+ </p>
+
+<div className='mt-8 '>
+    Now some extra info(unimportant) :
+</div>
+ <p className='italic mt-6'>
+
+
  The github code for this lives here : <a className='text-blue-600' href='https://github.com/mkpgtr/FormAssignmentInReactJs/tree/master/src'>
     Code
   </a>
 
-  <p className="italic mt-5">
-  Here is : <a className='text-red-600' href='/form'>The Form  </a>
   </p>
- </p>
+  <p className='italic'>
+ The github code for the components live here : <a className='text-blue-600' href='https://github.com/mkpgtr/FormAssignmentInReactJs/tree/master/src/components'>
+    Code
+  </a>
+
+  </p>
+  <p className='italic mt-8 underline font-semibold text-lg'>
+ Did not pay much attention to css here as this was a react assignment but if asked I will surely improve the look of this webapp
+ 
+
+  </p>
+
+  <p className='italic mt-8 font-semibold'>
+I focused mainly on creating reusable code with proper file/folder structures.
+used Context API to create a global state
+ 
+
+  </p>
+  <p className='italic mt-8 font-semibold text-2xl text-orange-600'>
+used tailwindcss to quickly style basic stuff
+ 
+
+  </p>
+
+  <p className='italic mt-8 text-lg'>
+The navbar component's use effect is inspired by my previous project (try resizing to see the magic) :
+Detailed use of contextAPI in my previous project 
+<a className='text-red-500' href="https://responsive-dashboard-by-manish.netlify.app/">That lives here</a>
+
+ 
+
+  </p>
+  
+
+  
 </div>
   )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -29,7 +29,12 @@ const Navbar = ({options,collapseAtBreakpoint}) => {
    
     <div ref={navRef} className={`flex justify-around gap-3 p-3 transition-all  text-white font-semibold ${collapseAtBreakpoint=='md' ? `md:flex-col`:`sm:flex-col ${collapsed? 'sm:d-block sm:p-0 sm:h-0 sm:overflow-hidden' : ``} `} cursor-pointer`}>
         {options.map((option)=>{
-          return <h1 key={option.id} onClick={()=>navigate(`${option.path}`)}>{option.label}</h1>
+          return <h1 key={option.id} onClick={()=>{
+            navigate(`${option.path}`)
+
+            // ! this will collapse the navbar when a nav item is clicked (a better user experience)
+            setCollapsed(true)
+          }}>{option.label}</h1>
         })}
     </div>
         </>
